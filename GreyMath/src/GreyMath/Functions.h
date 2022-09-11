b9188fd3bffe5bab8493cee8afc5a90ab2de9833
09GreyHate06 <83628097+09GreyHate06@users.noreply.github.com>
2022-09-11 10:49:55 +0800

    Dot product and vector projections

--- GreyMath/src/GreyMath/Functions.h
+++ GreyMath/src/GreyMath/Functions.h
@@ -18,10 +18,17 @@ namespace GreyMath
 		return v / Magnitude(v);
 	}
 
-	// ||P|| * ||Q|| * cos(a)
+	// ||lhs|| * ||rhs|| * cos(a)
 	float Dot(const Vector& lhs, const Vector& rhs)
 	{
 		return lhs.x * rhs.x + lhs.y * rhs.y + lhs.z * rhs.z + lhs.w * rhs.w;
+	}
+
+	// (v0 . v1) / ||v1||^2) * v1
+	Vector ProjectionOfV0OntoV1(const Vector& v0, const Vector& v1)
+	{
+		float v1MagSqrd = v1.x * v1.x + v1.y * v1.y + v1.z * v1.z + v1.w * v1.w;
+		return (Dot(v0, v1) / v1MagSqrd) * v1;
 	}
 
 
--- GreyMath/src/Main.cpp
+++ GreyMath/src/Main.cpp
@@ -4,13 +4,46 @@
 using namespace GreyMath;
 int main()
 {
-	Vector v0(0.5, 0.0f, 23.0f, 0.0f);
-	Vector v1(0.5, 0.0f, 23.0f, 0.0f);
+	Vector P(6.0f, -75.64f, 32.75f, 0.0f);
+	Vector Q(-123.2f, 23.12f, 90.5f, 0.0f);
+	Vector R(6.0f, -3.0f, 7.0f, 0.0f);
 
-	//v0 = Normalized(v0);
-	//v1 = Normalized(v1);
-	std::cout << Dot(5 * v0, v1) << '\n';
-	std::cout << 5 * Dot(v1, v0) << '\n';
+	std::cout << "=========== Dot Product Properties ===========\n";
+	std::cout << "P" << P << '\n';
+	std::cout << "Q" << Q << '\n';
+	std::cout << "R" << R << '\n';
+	std::cout << "a = 5\n\n";
+
+	std::cout << "P . Q = Q . P" << '\n';
+	std::cout << Dot(P, Q) << " = " << Dot(Q, P) << "\n\n";
+
+	std::cout << "(a P) . Q = a (P . Q)\n";
+	std::cout << Dot(5.0f * P, Q) << " = " << 5.0f * Dot(P, Q) << "\n\n";
+
+	std::cout << "P . (Q + R) = P . Q + P . R\n";
+	std::cout << Dot(P, Q + R) << " = " << Dot(P, Q) + Dot(P, R) << "\n\n";
+
+	std::cout << "P . P = ||P||^2\n";
+	std::cout << Dot(P, P) << " = " << powf(Magnitude(P), 2.0f) << "\n\n";
+
+	std::cout << "P . Q <= ||P|| ||Q||\n";
+	std::cout << Dot(P, Q) << " <= " << Magnitude(P) * Magnitude(Q) << '\n';
+
+
+	Vector U(6.0f, -3.0f, 9.0f, 0.0f);
+	Vector V(4.0f, -1.0f, 8.0f, 0.0f);
+	std::cout << "\n================= Projection =================\n";
+	std::cout << "U" << U << '\n';
+	std::cout << "V" << V << "\n\n";
+
+	Vector projUontoV = ProjectionOfV0OntoV1(U, V);
+
+	std::cout << "Projection of U onto V\n";
+	std::cout << projUontoV << "\n\n";
+
+	std::cout << "The vector component of U orthogonal to V\n";
+	std::cout << U - projUontoV << '\n';
+	std::cout << "Check: V . projUontoV = " << Dot(Normalized(V), Normalized(U - projUontoV)) << "\n\n"; // floating point error
 
 	return 0;
 }
